8ca78ad3e5b6242d149e3fe546a670e98fc93d39
Sasindu Janapriya <sasindujanapriya2002@gmail.com>
2025-02-06 19:57:15 +0530

    Create basic folder structure

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,10 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
@@ -29,7 +29,18 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+function MyButton() {
+  return <button> I am Sasindu</button>;
+}
+
+export default function MyApp() {
+  return (
+    <div>
+      <h1> Hello Peeps </h1>
+      <MyButton />
+    </div>
+  );
+}
